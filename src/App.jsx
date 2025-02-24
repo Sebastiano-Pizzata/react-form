951634dc6c69a2e48dcadd2bfed2359fbea701db
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-02-24 13:46:47 +0100

    ADD: form with no style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react"
+const initialArticles = ["Lotta al cambiamento climatico", "Tecnologia e privacy", "Crisi economica globale"]
 
 function App() {
 
+  const [articles, setArticles] = useState(initialArticles);
+  const [newArticles, setNewArticles] = useState("");
 
+  const addArticle = (e) => {
+    e.preventDefault();
+    const article = newArticles.trim()
+    setArticles([...articles, newArticles])
+    setNewArticles("")
+  }
   return (
     <>
+      <div>
+        <h1>Articoli Di Giornale</h1>
+        <ul className="list-group">
+          {
+            articles.map((element, index) => {
+              return (
+                <li key={index} className="list-group-item">{element}</li>
+              )
+            })
+          }
+        </ul>
+
+        {/* form nuovo articolo */}
+        <form onSubmit={addArticle}>
+          <input type="text"
+            className="form-control"
+            placeholder="Nuovo Articolo"
+            value={newArticles}
+            onChange={(e) => setNewArticles(e.target.value)} />
+        </form>
+      </div>
 
 
     </>
